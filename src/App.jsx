a3f09e7129bc06c771a55890f7d7895655d29ab0
Warachai Aranchai <tannywarachai@Warachais-MacBook-Air.local>
2025-12-19 11:46:28 +0700

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,15 @@
+import './App.css'
+import User from './components/User' 
+
+function App() {
+
+  return (
+    <>
+      <div className="card">
+        <p><User message="6610996 Worachai Aranchot"/></p>
+      </div>
+    </>
+  )
+}
+
+export default App
---
+++ src/components/User.jsx
@@ -0,0 +1,11 @@
+import "../App.css" 
+
+function User({message}) { 
+    return ( 
+        <div> 
+            <h1>Hello, React!</h1> 
+            <p>{message}</p>
+        </div> 
+    );  
+} 
+export default User; 
